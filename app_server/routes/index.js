2a5b16d3935cacf596095b32cae6fa9c33c10d6c
Gonz-inc <Glgonzalez247@gmail.com>
2024-07-01 22:26:15 -0400

    added the hover effect fixed endpoints routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,13 +5,12 @@ const ctrlAbout = require('../controllers/about');
 
 
 /* GET home page. */
+router.get('/', ctrlMain.index);
 router.get('/index', ctrlMain.index);
 
 /* GET the about pages */
 router.get('/about', ctrlAbout.about);
 router.get('/contact', ctrlAbout.contact);
 router.get('/projects', ctrlAbout.projects);
-
-
 
 module.exports = router;
